01ab7b7a406d127603b92acc4df637406adbe359
Michal Biedzki <mbiedzki@me.com>
2019-06-18 13:26:58 +0200

    module 4

--- src/main/webapp/angular2/src/app/app.module.ts
+++ src/main/webapp/angular2/src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/common/http";
+import {AppComponent} from "./app.component";
 
 @NgModule({
   declarations: [
@@ -11,10 +11,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
+    HttpModule,
     ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
